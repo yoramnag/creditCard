b3d40030138c1a2b0ff5c0f4b1f63980f613f034
Yoram Nagavker <48060702+yoramnag@users.noreply.github.com>
2022-07-07 11:21:50 +0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@ insert into BLACK_LIST(credit_card) values('5275320699756705');
 insert into BLACK_LIST(credit_card) values('5526667401765870');
 insert into BLACK_LIST(credit_card) values('5377240307952792');
 
-insert into TRANSACTIONS (id, credit_card , amount ,date) values(10001, '5394133851797797' , 100.00, to_date('07-07-2022','dd-MM-yyyy'));
+insert into TRANSACTIONS(id , credit_card , date ,amount) values(10001 , '5394133851797797',PARSEDATETIME('2020-08-07-00.00.00','yyyy-MM-dd-HH.mm.ss'),100.00);
