45c0ab1d13bf5df13e6c37f138ef3bec99632074
Yoram Nagavker <48060702+yoramnag@users.noreply.github.com>
2022-07-10 21:28:23 +0300

    add HTML file for black list

---
+++ src/main/java/com/example/creditCard/rest/DemoController.java
@@ -0,0 +1,44 @@
+package com.example.creditCard.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.creditCard.entity.BlackList;
+import com.example.creditCard.service.BlackListService;
+
+@Controller
+public class DemoController {
+	
+	@Autowired
+	private BlackListService blackListService;
+
+	// create a mapping for "/hello"
+	
+	@GetMapping("/hello")
+	public String sayHello(Model theModel) {
+		
+		theModel.addAttribute("theDate", new java.util.Date());
+		
+		return "helloworld";
+	}
+	
+	@GetMapping("/black-list-cards")
+	public String retrieveAllBlackListCards(Model theModel) {
+		List<BlackList> blackList = blackListService.findAll();
+		theModel.addAttribute("blaclListCards", blackList);
+		return "black-cards-list";
+		
+	}
+}
+
+
+
+
+
+
+
+
