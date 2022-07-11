f5957470bbd4413c9bb7c819db90711a148b140f
Yoram Nagavker <48060702+yoramnag@users.noreply.github.com>
2022-07-11 10:06:39 +0300

    add card form

--- src/main/java/com/example/creditCard/controller/BlackListController.java
+++ src/main/java/com/example/creditCard/controller/BlackListController.java
@@ -6,21 +6,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.creditCard.entity.BlackList;
 import com.example.creditCard.service.BlackListService;
 
 @Controller
+@RequestMapping("/blaclListHTML")
 public class BlackListController {
 	
 	@Autowired
 	private BlackListService blackListService;
 
 	@GetMapping("/black-list-cards")
-	public String retrieveAllBlackListCards(Model theModel) {
+	public String listOfBlackListCards(Model theModel) {
 		List<BlackList> blackList = blackListService.findAll();
 		theModel.addAttribute("blaclListCards", blackList);
-		return "blackListHTML/black-cards-list";
+		return "blackListHTMLPage/black-cards-list";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		// create model attribute to bind form data
+		BlackList blackList = new BlackList();
+		
+		theModel.addAttribute("blaclListCard", blackList);
+		return "blackListHTMLPage/black-card-form";
 		
 	}
 }
