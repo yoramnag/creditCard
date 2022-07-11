5d715444162ba35d82bb4f9346449229e3d40af6
yoramnag <48060702+yoramnag@users.noreply.github.com>
2022-07-11 11:44:25 +0300

    add new transaction

--- src/main/java/com/example/creditCard/controller/TransactionsController.java
+++ src/main/java/com/example/creditCard/controller/TransactionsController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.creditCard.entity.Transactions;
@@ -23,6 +25,22 @@ public class TransactionsController {
 		List<Transactions> transactionsList = transactionsService.findAll();
 		theModel.addAttribute("transactions", transactionsList);
 		return "transactionsHTMLPage/transactions-list";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		Transactions transaction = new Transactions();
+		theModel.addAttribute("newTransactions", transaction);
+		return "transactionsHTMLPage/transaction-form";
+	}
+	
+	@PostMapping("/save")
+	public String saveBlackListCard(@ModelAttribute("newTransactions") Transactions theTransaction) {
+		// save the employee
+		transactionsService.save(theTransaction);
+		
+		return "redirect:/transactionsHTML/transactions-list";
 	}
 
 }
